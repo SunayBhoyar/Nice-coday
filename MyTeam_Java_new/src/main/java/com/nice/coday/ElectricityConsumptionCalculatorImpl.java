20ffe28b1bacb1f50b9ad8e947ad05c494deb8ae
LethalAntiD0te <rishibalapure12@example.com>
2024-09-14 12:45:19 +0530

    csv parser implemented

--- MyTeam_Java_new/src/main/java/com/nice/coday/ElectricityConsumptionCalculatorImpl.java
+++ MyTeam_Java_new/src/main/java/com/nice/coday/ElectricityConsumptionCalculatorImpl.java
@@ -4,11 +4,22 @@ package com.nice.coday;
 import org.apache.poi.ss.formula.eval.NotImplementedException;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 public class ElectricityConsumptionCalculatorImpl implements ElectricityConsumptionCalculator {
+    public Map<String, Double> chargingStationData;
+    public Map<String, Double> entryExitPointData;
+    public Map<String, VehicleInfo> vehicleTypeData;
+    public Map<String, Map<String, Double>> timeToChargeData;
+    public List<TripDetails> tripDetails;
     @Override
     public ConsumptionResult calculateElectricityAndTimeConsumption(ResourceInfo resourceInfo) throws IOException {
-        // Your implementation will go here
-        throw new NotImplementedException("Not implemented yet.");
+        chargingStationData=CSVParser.readChargingStationData(resourceInfo.chargingStationInfoPath);
+        entryExitPointData=CSVParser.readEntryExitPointData(resourceInfo.entryExitPointInfoPath);
+        vehicleTypeData=CSVParser.readVehicleTypeData(resourceInfo.vehicleTypeInfoPath);
+        timeToChargeData=CSVParser.readTimeToChargeData(resourceInfo.timeToChargeVehicleInfoPath);
+        tripDetails=CSVParser.readTripDetails(resourceInfo.tripDetailsPath);
+        
     }
 }
